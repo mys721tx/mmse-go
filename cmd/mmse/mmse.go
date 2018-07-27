fa09d85b1043b0bbe955534b2e1aede7b14f95a8
Yishen Miao <mys721tx@gmail.com>
2018-07-26 21:42:45 -0400

    Implementing packer
    Implements packer. Error handling is simplified. Unrecoverable error
    will panic in place.

--- cmd/mmse/mmse.go
+++ cmd/mmse/mmse.go
@@ -71,59 +71,102 @@ func (f *frame) decode() error {
 	return err
 }
 
+func (f *frame) encode() error {
+	b := make([]byte, f.sizeRaw)
+
+	n, err := lz4.CompressBlock(f.Bytes(), b, make([]int, 1<<16))
+
+	if err != nil {
+		return err
+	}
+
+	// lz4.CompressBLock returns 0 if the data is not compressible.
+	if n == 0 {
+		f.sizeCom = f.sizeRaw
+	} else {
+		f.sizeCom = int32(n)
+	}
+
+	f.Reset()
+
+	_, err = f.Write(b)
+
+	f.Truncate(int(f.sizeCom))
+
+	return err
+}
+
 // readInt32 reads an int32 from a file.
 func readInt32(r io.Reader) (int32, error) {
 	var v int32
 
-	err := binary.Read(r, binary.LittleEndian, &v)
-
-	if err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &v); err != nil {
 		return 0, err
 	}
 
 	return v, nil
 }
 
-// readToFrame returns a buffer for lz4 to use
-func readToFrame(r io.Reader) (*frame, error) {
-	enc, err := readInt32(r)
+// writeInt32 writes an int32 from a file.
+func writeInt32(w io.Writer, v int32) error {
 
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read encoded size: %s", err)
-	}
+	err := binary.Write(w, binary.LittleEndian, v)
 
-	unc, err := readInt32(r)
-
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read unencoded size: %s", err)
-	}
-
-	return &frame{sizeRaw: unc, sizeCom: enc}, nil
+	return err
 }
 
-// checkMagic checks the magic number in the save file.
-func checkMagic(r io.Reader) {
-	m, err := readInt32(r)
+// readSizeToFrame reads the sizes of lz4 blocks from a file and returns a
+// frame.
+func readSizeToFrame(r io.Reader) *frame {
+	f := new(frame)
 
-	if err != nil {
-		log.Panicf("Failed magic number check: %s", err)
+	if enc, err := readInt32(r); err != nil {
+		log.Panicf("Unable to read encoded size: %s", err)
+	} else {
+		f.sizeCom = enc
 	}
 
-	if m != magic {
+	if unc, err := readInt32(r); err != nil {
+		log.Panicf("Unable to read unencoded size: %s", err)
+	} else {
+		f.sizeRaw = unc
+	}
+
+	return f
+}
+
+// readJSONToFrame reads from a file into a frame, compresses it, and sets the
+// sizes.
+func readJSONToFrame(fn string) *frame {
+	f := new(frame)
+
+	if r, err := os.Open(fn); err != nil {
+		log.Panicf("Unable to open json file: %s", err)
+	} else if n, err := io.Copy(f, r); err != nil {
+		log.Panicf("Unable to read json file: %s", err)
+	} else {
+		f.sizeRaw = int32(n)
+	}
+
+	if err := f.encode(); err != nil {
+		log.Panicf("Unable to compress frame: %s", err)
+	}
+
+	return f
+}
+
+// checkHeader checks the magic number and version number in the save file.
+func checkHeader(r io.Reader) {
+	if m, err := readInt32(r); err != nil {
+		log.Panicf("Failed magic number check: %s", err)
+	} else if m != magic {
 		log.Panicf("Incorrect magic number: %d", m)
 	}
-}
 
-// checkVer checks the version number in the save file.
-func checkVer(r io.Reader) {
-	v, err := readInt32(r)
-
-	if err != nil {
+	if v, err := readInt32(r); err != nil {
 		log.Panicf("Failed version number check: %s", err)
-	}
-
-	if v != ver {
-		log.Panicf("Failed version number check: %x", v)
+	} else if v != ver {
+		log.Panicf("Incorrect version number: %x", v)
 	}
 }
 
@@ -143,13 +186,41 @@ func writeJSON(fn string, r io.Reader, f *frame) {
 	}
 }
 
+// writeHeader writes the magic number and version number to a save file.
+func writeHeader(w io.Writer) {
+	if err := writeInt32(w, magic); err != nil {
+		log.Panicf("Unable to write magic number: %s", err)
+	}
+	if err := writeInt32(w, ver); err != nil {
+		log.Panicf("Unable to write version number: %s", err)
+	}
+}
+
+// writeSize writes size to a save file.
+func writeSize(w io.Writer, f *frame) {
+	if err := writeInt32(w, f.sizeCom); err != nil {
+		log.Panicf("Unable to write encoded size: %s", err)
+	}
+
+	if err := writeInt32(w, f.sizeRaw); err != nil {
+		log.Panicf("Unable to write unencoded size: %s", err)
+	}
+}
+
+// writeFrame writes the frame to a save file.
+func writeFrame(w io.Writer, f *frame) {
+	if _, err := io.Copy(w, f); err != nil {
+		log.Panicf("Unable to write frame to save file: %s", err)
+	}
+}
+
 // split splits a file name into base and extension. Modified from path.Ext().
 func split(fn string) string {
 	for i := len(fn) - 1; i >= 0; i-- {
 		if fn[i] == '.' {
 			if fn[i:] == ".sav" || fn[i:] == ".json" {
-			return fn[:i]
-		}
+				return fn[:i]
+			}
 			break
 		}
 	}
@@ -172,21 +243,44 @@ func unpack(fn string) {
 		}
 	}()
 
-	checkMagic(f)
-	checkVer(f)
+	checkHeader(f)
 
-	info, err := readToFrame(f)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	info := readSizeToFrame(f)
 
-	data, err := readToFrame(f)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	data := readSizeToFrame(f)
 
 	writeJSON(fmt.Sprintf("%s_info.json", bn), f, info)
 	writeJSON(fmt.Sprintf("%s_data.json", bn), f, data)
+}
+
+// unpack is a wrapper for packing json files.
+func pack(in, dn string) {
+	bn := split(path.Base(dn))
+
+	f, err := os.Create(fmt.Sprintf("%s.sav", bn))
+
+	if err != nil {
+		log.Panicf("%s", err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Panicf("Unable to close file: %s", err)
+		}
+	}()
+
+	writeHeader(f)
+
+	info := readJSONToFrame(in)
+
+	writeSize(f, info)
+
+	data := readJSONToFrame(dn)
+
+	writeSize(f, data)
+
+	writeFrame(f, info)
+	writeFrame(f, data)
 }
 
 func main() {
@@ -197,7 +291,7 @@ func main() {
 		unpack(os.Args[1])
 	case 3:
 		// pack when os.Args has two files
-		fmt.Println(os.Args[1:])
+		pack(os.Args[1], os.Args[2])
 	default:
 		// print usage in other case
 		fmt.Printf(usg, os.Args[0])
